57a37efa9f168beb6764f4234c8f1fd7ee755471
merajuni7ed <merajmusavi1243m@gmail.com>
2022-11-26 22:50:16 +0330

    add item to recyclerview feature

--- app/src/main/java/com/example/myapplication/Adapter.java
+++ app/src/main/java/com/example/myapplication/Adapter.java
@@ -9,29 +9,35 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class Adapter  extends RecyclerView.Adapter<Adapter.ViewHolder> {
-    private String[] contacts = new String[20];
+    private ArrayList<String> contacts = new ArrayList<>();
     public Adapter(){
-        contacts[0] = "Ruthann Trustrie";
-        contacts[1] = "Peadar Dawtrey";
-        contacts[2] = "Felipe Bradtke";
-        contacts[3] = "Claude Crissil";
-        contacts[4] = "Jacky Girardeau";
-        contacts[5] = "Rubia Dominguez";
-        contacts[6] = "Michaela Churchley";
-        contacts[7] = "Harvey Pentelow";
-        contacts[8] = "Neilla Langton";
-        contacts[9] = "Marco Greaves";
-        contacts[10] = "Liz Batchley";
-        contacts[11] = "Lamond Littlepage";
-        contacts[12] = "Malina Weir";
-        contacts[13] = "Tomlin Lenchenko";
-        contacts[14] = "Hy Pavelin";
-        contacts[15] = "Jenelle Palin";
-        contacts[16] = "Damon Knewstubb";
-        contacts[17] = "Alex Ivanusyev";
-        contacts[18] = "Hamil Callery";
-        contacts[19] = "Karol Syer";
+        contacts.add("Ruthann Trustrie");
+        contacts.add("Peadar Dawtrey");
+        contacts.add("Felipe Bradtke");
+        contacts.add("Claude Crissil");
+        contacts.add("Jacky Girardeau");
+        contacts.add("Rubia Dominguez");
+        contacts.add("Michaela Churchley");
+        contacts.add("Harvey Pentelow");
+        contacts.add("Neilla Langton");
+        contacts.add("Marco Greaves");
+        contacts.add("Liz Batchley");
+        contacts.add("Lamond Littlepage");
+        contacts.add("Malina Weir");
+        contacts.add("Tomlin Lenchenko");
+        contacts.add("Hy Pavelin");
+        contacts.add("Jenelle Palin");
+        contacts.add("Damon Knewstubb");
+        contacts.add("Alex Ivanusyev");
+        contacts.add("Hamil Callery");
+        contacts.add("Karol Syer");
+    }
+    public void addContact(String name){
+        contacts.add(0,name);
+        notifyItemInserted(0);
     }
     @NonNull
     @Override
@@ -42,12 +48,12 @@ public class Adapter  extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
- holder.bindcontact(contacts[position]);
+ holder.bindcontact(contacts.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return contacts.length;
+        return contacts.size();
     }
 
 
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,16 +5,35 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView addCTC;
+ EditText et_add_CTC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Adapter adapter = new Adapter();
+        addCTC = findViewById(R.id.iv_add_ctc);
+        et_add_CTC = findViewById(R.id.ctc_f_name);
+
         RecyclerView recyclerView = findViewById(R.id.re);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
         recyclerView.setAdapter(adapter);
+        addCTC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+          if (et_add_CTC.length()>0){
+              adapter.addContact(et_add_CTC.getText().toString());
+              recyclerView.scrollToPosition(0);
+          }else{
+              Toast.makeText(MainActivity.this, "edit text cant be empty", Toast.LENGTH_SHORT).show();
+          }
+            }
+        });
     }
 }
